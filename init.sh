cc7105a25f51b8bd4a06db6ee61bdb1292db0d09
a.kukuev <a.kukuev@corp.mail.ru>
2016-10-04 02:05:09 +0300

    go back

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-sudo nginx -s stop
-sudo rm /etc/nginx/sites-avalible/default
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf $PWD/etc/nginx.conf /etc/nginx/conf.d/squary.conf
-
-sudo nginx
