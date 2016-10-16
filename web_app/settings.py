ef7bed7f6c354b18c9ae5bdf36a38b7570ea6695
a.kukuev <a.kukuev@corp.mail.ru>
2016-10-16 22:53:16 +0300

    add model User

--- web_app/settings.py
+++ web_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../collected_static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"), )
+
+AUTH_USER_MODEL = 'posts.User'
