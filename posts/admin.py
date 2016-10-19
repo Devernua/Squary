ec46f235913d40643157648a4a9a8b2b324f5658
a.kukuev <a.kukuev@corp.mail.ru>
2016-10-19 12:56:15 +0300

    with models

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,8 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import User, Image, Comment
 
 
 # Register your models here.
@@ -11,15 +11,17 @@ from .models import User
 class UserAdmin(BaseUserAdmin):
 
     fieldsets = BaseUserAdmin.fieldsets + (
-        (u'Дополнительно', {'fields': ('admin_avatar', 'avatar')}),
+        (u'Дополнительно', {'fields': [ 'avatar']}),
     )
 
-    def admin_avatar(self, instance):
-        return instance.avatar and u'<img src="{0}" width="100px" />'.format(
-            instance.avatar.url
-        )
-    admin_avatar.allow_tags = True
-    admin_avatar.short_description = u'Аватар'
+    #def admin_avatar(self, instance):
+    #    return instance.avatar and u'<img src="{0}" width="100px" />'.format(
+    #        instance.avatar.url
+    #    )
+    #admin_avatar.allow_tags = True
+    #admin_avatar.short_description = u'Аватар'
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Image)
+admin.site.register(Comment)
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,45 @@
-from __future__ import unicode_literals
+# coding: utf-8
+from __future__ 		import unicode_literals
 
-from django.db import models
+from django.conf 		import settings
+from django.db 			import models
 from django.contrib.auth.models import AbstractUser
+from django.utils 		import timezone
 
+import datetime
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
 
-# Create your models here.
+class Image(models.Model):
+	author 	= models.ForeignKey(settings.AUTH_USER_MODEL)
+	name 	= models.CharField(max_length = 200)
+	img 	= models.ImageField(upload_to='posts', blank=True, null=True)
+	pdate 	= models.DateTimeField('date published');
+	rating 	= models.IntegerField(default=0);
+	
+	def __str__(self):
+		return self.name
+	
+	def was_published_recently(self):
+		return self.pdate >= timezone.now() - datetime.timedelta(days = 1)
+	
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
+
+class Comment(models.Model):
+	author	= models.ForeignKey(settings.AUTH_USER_MODEL)
+	image 	= models.ForeignKey(Image)
+	text  	= models.CharField(max_length=2000)
+	pdate 	= models.DateTimeField('date published')
+	
+	def __str__(self):
+		return self.text;
+
+	def was_published_recently(self):
+		return self.pdate >= timezone.now() - datetime.timedelta(days = 1)
+	
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
